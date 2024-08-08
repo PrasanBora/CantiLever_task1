75586a664ff108d7e2fc4a58024653fb0ceb057e
Prasan Bora <prasanbora23@gmail.com>
2024-08-08 12:26:41 +0530

    js

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -13,18 +13,30 @@ import cart_icon from '../Assets/cart_icon.png'
         <p>SHOP</p>
        </div>
 
-       <ul className='nav_menu'>
-         <li onClick={()=>{setMenu("shop")}} >Shop{menu==="shop"?<hr/>:<></>} </li>
-         <li onClick={()=>{setMenu("mens")}} >Men {menu==="mens"?<hr/>:<></>} </li>
-         <li onClick={()=>{setMenu("womens")}} >Women {menu==="womens"?<hr/>:<></>} </li>
-         <li onClick={()=>{setMenu("kids")}} >Kids {menu==="kids"?<hr/>:<></>} </li>
-       </ul>
+       <ul className={Classes.nav_menu}>
+
+        <li onClick={() => { setMenu("shop");}} >
+          <Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu ==="shop"?<hr/>:<></>}
+        </li>
+
+        <li onClick={() => { setMenu("mens");}} >
+          <Link style={{textDecoration: 'none'}} to='/mens'>Men</Link> {menu ==="mens"?<hr/>:<></>}
+        </li>
+
+        <li onClick={() => { setMenu("womens");}} >
+          <Link style={{textDecoration: 'none'}} to='/womens'>Women</Link> {menu ==="womens"?<hr/>:<></>}
+        </li>
+
+        <li onClick={() => { setMenu("kids");}} >
+          <Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link> {menu ==="kids"?<hr/>:<></>}
+        </li>
+
+      </ul>
 
        <div className='cart'>
-         <button>Login </button>
-         <img src = {cart_icon} alt ="cart" />
+          <Link to = '/login'> <button>Login </button> </Link>
+          <Link to = '/cart'> <img src = {cart_icon} alt ="cart" />  </Link>
        </div>
-
 
     </div>
   )
--- frontend/src/pages/shop.js
+++ frontend/src/pages/shop.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export const shop = () => {
   return (
-    <div>shop</div>
+    <div></div>
   )
 }
