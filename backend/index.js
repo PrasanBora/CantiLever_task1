8c653577a2a12aa8064e37ebd35711ec0ada156a
Prasan Bora <prasanbora23@gmail.com>
2024-09-05 12:04:55 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -16,18 +16,16 @@ const url = process.env.MONGO_URL;
 app.use(express.json());
 app.use(cors());
 
-
+//db 
 mongoose.connect(url);
 
 //api creation
-
 app.get("/",(req,res)=>{
     res.send("Expess is running");
     
 })
 
 // image storage 
-
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename : (req,file,cb)=>{
@@ -38,8 +36,6 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage})
 
 // upload endpoint 
-
-
 app.use('/images',express.static('upload/images'))
 
 app.post("/upload",upload.single('product'), (req,res)=>{
@@ -84,6 +80,60 @@ const Product = mongoose.model("Product",{
           default:true,
         },  
 })
+
+app.post('/addproduct',async (req,res)=>{
+  let products = await Product.find({});
+  let id;
+  if(products.length>0)
+  {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id+1;
+  }
+  else
+  {
+    id=1;
+  }
+  const product = new Product({
+    id:id,
+    name:req.body.name,
+    image:req.body.image,
+    category:req.body.category,
+    new_price:req.body.new_price,
+    old_price:req.body.old_price,
+
+  });
+
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  res.json({
+    success:true,
+    name:req.body.name,
+  })
+})
+
+//For deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+  await Product.findOneAndDelete({id:req.body.id});
+  console.log("Removed");
+  res.json({
+    success:true,
+    name:req.body.name
+  })
+})
+
+//for getting all products
+
+app.get('/allproducts',async(req,res)=>{
+  let products = await Product.find({});
+  console.log("All Products Fetched");
+  res.send(products);
+}) 
+
+
+
 
 app.listen(port,(error)=>{
     if(!error)
